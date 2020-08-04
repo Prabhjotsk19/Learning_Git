a60a1150267c75b506d8244901970e0da9f41e3a
Prabhjot Singh <starcoder19@gmail.com>
2020-08-04 10:38:56 +0000

    git-remote and git-push

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-# Git Commands
-# git push
+# Git Commands ->
+# git init
+# git add 
+# git status
+# git commit -m "messsge with new commit"
+# git log
+# git remote add origin online-git-repository-URL
+# git push -u origin master
+# git pull
+
