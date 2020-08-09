e8f942141e73cfdd53fb7c7e073496b9d990fb87
Prabhjot Singh <starcoder19@gmail.com>
2020-08-09 04:34:27 +0000

    git log modified

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # git add 
 # git status
 # git commit -m "messsge with new commit"
-# git log
+# git log press 'q' to exit
 # git remote add origin online-git-repository-URL
 # git push -u origin master
 # git pull
